a0e3750d9fc7cd98cb6c2c61ddfea7451b44d419
Lucas Rios <lucasfaesa@gmail.com>
2024-07-06 11:01:40 -0300

    created initial fade in, imported dotween package

---
+++ Assets/_Game/Misc/Scripts/FadeController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeController : MonoBehaviour
+{
+    [SerializeField] private Image _blackFade;
+    [Space] 
+    [SerializeField] private bool _canFade = true;
+    [SerializeField] private float _fadeTime = 1f;
+
+    private float elapsedTime;
+    
+    private void Awake()
+    {
+        _blackFade.color = Color.black;
+        FadeOut();
+    }
+
+    private void FadeOut()
+    {
+        _blackFade.DOFade( 0f, _fadeTime);
+    }
+}
